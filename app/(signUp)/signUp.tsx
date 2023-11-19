7757be1df75df41113eb832b2034f2e584ab44c3
dbalestieri <dbalestieri@mulesoft.com>
2023-11-19 16:41:39 -0300

    add error handling for request

--- app/(signUp)/signUp.tsx
+++ app/(signUp)/signUp.tsx
@@ -1,15 +1,21 @@
+import React, { useState } from 'react';
 import { SignUpScreen } from '../../screens/SignUpScreen';
+import { Snackbar } from 'react-native-paper';
 const config = require('../../config.json');
 
 export default function Page() {
+    const [errorMessage, setErrorMessage] = useState('');
+    const [successMessage, setSuccessMessage] = useState('');
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [showErrorMessage, setShowErrorMessage] = useState(false);
+
     const handleSingUpSubmit = (email: string, password: string) => {
         const body = {
             "email": email,
             "password": password,
             "validationCode": "123"
         }
-        console.log(body);
-        fetch(config.backend + "/users/register", {
+        fetch(config.api.url + "/users/register", {
             method: 'POST',
             headers: {
                 Accept: 'application/json',
@@ -17,15 +23,48 @@ export default function Page() {
             },
             body: JSON.stringify(body)
         })
-        .then(response => response.json())
-        .then(json => {
-            console.log(json);
-        })
-        .catch(error => {
-            console.log(error);   
+        .then(response => {
+            if (!response.ok){
+                const contentType = response.headers.get('content-type');
+                if(contentType && contentType.includes('application/json')) {
+                    response.json().then(json => {
+                        let err = new Error(json["error"] || "F");
+                        throw err;
+                    })
+                    .catch(error => {
+                        setShowErrorMessage(true);
+                        setShowSuccessMessage(false);
+                        setErrorMessage(error.message);
+                    })
+                }
+                else {
+                    setShowErrorMessage(true);
+                    setShowSuccessMessage(false);
+                    setErrorMessage(config.api.defaultErrorMessage);
+                }
+            } else {
+                response.json().then(json => {
+                    console.log(json)
+                    setShowSuccessMessage(true);
+                    setShowErrorMessage(false);
+                })
+            }
         })
     }
     return (
-        <SignUpScreen onNext={handleSingUpSubmit}/>
+        <>
+            <SignUpScreen onNext={handleSingUpSubmit}/>
+            <Snackbar
+                visible={showSuccessMessage}
+                onDismiss={() => {setShowSuccessMessage(false)}}>
+                Success
+            </Snackbar>
+            <Snackbar
+                visible={showErrorMessage}
+                onDismiss={() => {setShowErrorMessage(false)}}
+                >
+                {errorMessage}
+            </Snackbar>
+        </>
     );
 }

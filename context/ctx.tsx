a3aab33b4a31287442e3c30728c6b8377ac75000
Sebastián Capelli <scapelli@fi.uba.ar>
2023-12-21 11:10:12 -0300

    add comments to ctx file

--- context/ctx.tsx
+++ context/ctx.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import { useStorageState } from './useStorageState';
-import { isLoading } from 'expo-font';
 import { Buffer } from "buffer"
 
-// Context values are declare here
+
 const AuthContext = React.createContext<{ 
     signIn: (accessToken: string, refreshToken: string) => void; 
     signOut: () => void; 
+    processTokens: (accessToken: string, refreshToken: string) => void,
     accessToken?: string | null,
     refreshToken?: string | null,
     isLoading: boolean,
     
-    processTokens: (accessToken: string, refreshToken: string) => void,
+    // Context data is declared here
+    // ---------- ---------- ---------- ---------- ----------
     username: string,
     email: string,
-    
+    // ---------- ---------- ---------- ---------- ----------
 } | null> (null);
 
-// This hook can be used to access the user info.
+
 export function useSession() {
     const value = React.useContext(AuthContext);
     if (process.env.NODE_ENV !== 'production') {
@@ -39,19 +40,23 @@ export function SessionProvider(props: SessionProviderProps) {
 
     const isLoading = isLoading_AT && isLoading_RT
 
+    // Context data seters are declared here
+    // ---------- ---------- ---------- ---------- ----------
     const [username, setUsername] = React.useState<string>("");
     const [email, setEmail] = React.useState<string>("");
+    // ---------- ---------- ---------- ---------- ----------
 
-    // Tokens are received and saved in the context. 
-    // The relevant information from their payloads are alse saved in the context 
     const processTokens = (accessToken: string, refreshToken: string) => {
         const parts = accessToken.split('.').map(
             part => Buffer.from(part.replace(/-/g, '+').replace(/_/g, '/'), 'base64').toString()
         );
         const payload = JSON.parse(parts[1]);
 
-        setUsername("PENDIENTE");
+        // Context data is initialized here
+        // ---------- ---------- ---------- ---------- ----------
+        setUsername(payload.username);
         setEmail(payload.email);
+        // ---------- ---------- ---------- ---------- ----------
     }
 
     const values = {
@@ -71,8 +76,12 @@ export function SessionProvider(props: SessionProviderProps) {
         isLoading,
 
         processTokens,
+
+        // Context data is shared from here
+        // ---------- ---------- ---------- ---------- ----------
         username, 
         email
+        // ---------- ---------- ---------- ---------- ----------
     }
 
     return (

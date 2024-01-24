267b94aeb3e5d8b0950288042d6d15f4762f97c3
dbalestieri <dbalestieri@mulesoft.com>
2024-01-23 21:56:24 -0300

    change text of not available

--- screens/MovieDetailScreen.tsx
+++ screens/MovieDetailScreen.tsx
@@ -80,7 +80,7 @@ const renderPlatforms = (params: MovieDetailScreenParams) => {
                     </View>
                 </View>
             </> : 
-            <BodyText size='big' color={colors.primaryRed} body='La pelicula no se encuentra disponible' style={{width: 180, margin: 10}} />    
+            <BodyText size='big' color={colors.primaryRed} body='No disponible en ninguna plataforma' style={{width: 180, margin: 10}} />    
         }
     </View>
     )

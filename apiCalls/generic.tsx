bc967faa624c3248973af46aea082017ca03e83a
Sebastián Capelli <scapelli@fi.uba.ar>
2024-02-06 01:46:10 -0300

    refresh token implemented with bugs

--- apiCalls/generic.tsx
+++ apiCalls/generic.tsx
@@ -4,6 +4,8 @@ import { useSession } from '../context/ctx';
 import config from '../config.json';
 
 const baseURL = config.api.baseUrl
+const tokenExpiredErrorMessage = "Invalid auth token"
+const tokenExpiredStatusCode = 401
 
 // --------- --------- --------- --------- --------- ---------
 // From Axios Documentation:
@@ -20,6 +22,64 @@ export type Params = {
 }
 // --------- --------- --------- --------- --------- ---------
 
+function onSuccessRefresh (privateCallParams: any, response: AxiosResponse<any, any>) {
+    const signIn = privateCallParams.session?.signIn
+    const accessToken = response.data.token
+    const refreshToken = response.data.refreshToken
+    
+    console.log('Token refreshed')
+    console.log('New access token: ' + accessToken)
+    console.log('New refresh token: ' + refreshToken)
+
+    signIn?.(accessToken, refreshToken)
+
+    privateCall(
+        privateCallParams.method,
+        privateCallParams.session,
+        privateCallParams.endpoint,
+        privateCallParams.paramsAndData,
+        privateCallParams.onSuccess,
+        privateCallParams.onFailure,
+        accessToken
+    )
+    
+}
+
+function refreshTokens(privateCallParams: any){
+    const refreshToken = privateCallParams.session?.refreshToken
+    const data = { refreshToken }
+    const endpoint = '/auth/refreshCredentials'
+
+    axios({
+        method: 'POST',
+        url: baseURL + endpoint,
+        responseType: 'json',
+        data: data
+    })
+        .then(
+            (response) => {
+                onSuccessRefresh(privateCallParams, response)
+            }, (error) => {
+                // TODO: Acá se implementaría la logica de Refresh token vencido (o bien podemos definir usar refresh tokens sin vencimiento)
+                privateCallParams.onFailure(error)
+            });
+}
+
+
+function expiredToken(response: any) {
+    return response.status === tokenExpiredStatusCode && response.data.error === tokenExpiredErrorMessage
+}
+
+function handlePrivateCallError(
+    error: any,
+    privateCallParams: any,
+) {
+    if (expiredToken(error.response)) {
+        refreshTokens(privateCallParams)
+    } else {
+        privateCallParams.onFailure(error)
+    }
+}
 
 // Use for calls that require a token
 export function privateCall(
@@ -28,14 +88,16 @@ export function privateCall(
     endpoint: string,
     paramsAndData: Params,
     onSuccess: (response: AxiosResponse<any, any>) => void,
-    onFailure: (error: any) => void
+    onFailure: (error: any) => void,
+    newAccessToken?: string
 ) {
     const accessToken = session?.accessToken
     axios({
         method: method,
         url: baseURL + endpoint,
         responseType: 'json',
-        headers: { "Authorization": `Bearer ${accessToken}`},
+        // headers: { "Authorization": `Bearer ${accessToken}`},
+        headers: { "Authorization": `Bearer ${newAccessToken || accessToken}`},
         params: paramsAndData.params,
         data: paramsAndData.data
     })
@@ -43,8 +105,8 @@ export function privateCall(
             (response) => {
                 onSuccess(response)
             }, (error) => {
-                // TODO: LOGICA DE TOKEN comienza acá (¡pero se debe modularizar!)
-                onFailure(error)
+                const privateCallParams = { method, session, endpoint, paramsAndData, onSuccess, onFailure }
+                handlePrivateCallError(error, privateCallParams)
             });
 }
 

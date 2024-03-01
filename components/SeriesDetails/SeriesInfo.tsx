0bc382b42f11a20e8417a8cece1fea696fc7c07f
dbalestieri <dbalestieri@mulesoft.com>
2024-03-01 10:11:18 -0300

    series details refactor

---
+++ components/SeriesDetails/SeriesInfo.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { SeriesDetail } from '../../entities/Details/Series/SeriesDetailEntry';
+import { ImageBackground, LayoutChangeEvent, View } from 'react-native';
+import { styles } from './styles/SeriesDetails.styles';
+import { colors } from '../../assets';
+import { TitleText } from '../BasicComponents/TitleText';
+import { BodyText } from '../BasicComponents/BodyText';
+import { getScreenSize } from '../../utils/screenUtils';
+import { TmdbImage, TmdbImageType } from '../BasicComponents/TmdbImage';
+
+type SeriesInfoParams = {
+    series: SeriesDetail;
+}
+
+export const SeriesInfo = (params: SeriesInfoParams) => {
+    const [titleTextHeight, setTitleTextHeight] = useState(0);
+    
+    const handleTitleTextLayout = (event: LayoutChangeEvent) => {
+        setTitleTextHeight(event.nativeEvent.layout.height);
+    };
+
+    const releaseYear = params.series.releaseDate? params.series.releaseDate.getFullYear() : " ? ";
+    const lastYear = params.series.lastAirDate? params.series.lastAirDate.getFullYear() : " ? ";
+    const screenWidth = getScreenSize().width;
+    const backgroundSize = 210 + (titleTextHeight/30 - 2)*30
+
+    return(
+        <ImageBackground
+                source={{ uri: "https://image.tmdb.org/t/p/original" + params.series.backdrop }} 
+                style={[styles.backdropImage, {height: backgroundSize}]}
+                resizeMode="cover"
+            >
+            {params.series.backdrop?
+                <View style={[styles.darkness, {height: backgroundSize}]} /> :
+                <View style={[styles.darkness, {height: backgroundSize, backgroundColor: colors.primarySkyBlue + '85'}]} />
+            }
+            <View style={styles.textOverlay}>
+                <TitleText
+                    body={params.series.title} 
+                    size='big' 
+                    style={{width: screenWidth - 10, fontWeight: 'bold'}} 
+                    onLayout={handleTitleTextLayout}
+                />
+                {(params.series.status === 'Finalizada' || params.series.status === 'Cancelada')?
+                <TitleText 
+                    body={"(" + releaseYear + ' - ' + (lastYear? lastYear : " ? ") + ")"} 
+                    size='big' 
+                    style={{width: screenWidth - 10, fontWeight: 'bold'}} /> : null}
+                {params.series.status === 'Serie en emisión'?
+                <TitleText 
+                    body={"(" + (releaseYear? releaseYear : " ? ") + ' - Presente)'} 
+                    size='big' 
+                    style={{width: screenWidth - 10, fontWeight: 'bold'}}/> : null}
+                <BodyText body={'Cant. espisodios: ' + params.series.totalEpisodes} size='medium' style={{fontWeight: 'bold'}} />
+                <BodyText body={'Cant. temporadas: ' + params.series.totalSeasons} size='medium' style={{fontWeight: 'bold'}} />
+                <BodyText body={'Creador: ' + params.series.createdBy[0]} size='medium' style={{fontWeight: 'bold'}} />
+            </View>
+            <View style={[styles.imageOverlay, {top: backgroundSize - 90}]}>
+                <TmdbImage
+                    resource={params.series.poster}
+                    type={TmdbImageType.Cover}
+                    style={styles.posterImage} />
+            </View>
+        </ImageBackground>
+    )
+}
--- components/SeriesDetails/styles/SeriesDetails.styles.tsx
+++ components/SeriesDetails/styles/SeriesDetails.styles.tsx
@@ -1,5 +1,8 @@
 import { StyleSheet} from 'react-native'
 import { colors } from '../../../assets';
+import { getScreenSize } from '../../../utils/screenUtils';
+
+const screenWidth = getScreenSize().width;
 
 export const styles = StyleSheet.create({
     platforms: {
@@ -49,5 +52,31 @@ export const styles = StyleSheet.create({
         width: 150,
         height: 230,
         borderRadius: 20
-    }
+    },
+    backdropImage: {
+        width: screenWidth,
+        height: 210
+    },
+    darkness: {
+        backgroundColor: colors.secondaryWhite + '85',
+        width: screenWidth
+    },
+    textOverlay: {
+        position: 'absolute',
+        top: 10,
+        left: 5,
+        alignSelf: 'center',
+    },
+    posterImage: {
+        width: 170,
+        height: 255,
+        marginRight: 10,
+        borderWidth: 2,
+        borderColor: colors.primaryBlack,
+    },
+    imageOverlay: {
+        position: 'absolute',
+        top: 90,
+        alignSelf: 'flex-end'
+    },
 });

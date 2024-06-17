dedd9a0a4d3a56135bae939089cbac43dbc26e83
Sebastián Capelli <scapelli@fi.uba.ar>
2024-06-17 03:10:22 -0300

    umr recos ok

--- components/Recos/UserRecos.tsx
+++ components/Recos/UserRecos.tsx
@@ -1,18 +1,62 @@
 import React from 'react'
-import { StyleSheet, ScrollView } from 'react-native'
+import { StyleSheet, ScrollView, View, Text, Image, Pressable } from 'react-native'
 import { useAppSelector } from '../../hooks/redux/useAppSelector'
 import { UserReco } from './UserReco'
+import { LocalIcon } from '../Types/LocalIcon'
+
+const hasIndex = (list: any[], index: number) => {
+    return index >= 0 && index < list.length;
+};
+
+
+
+
 
 export const UserRecos = () => {
     const { userRecos } = useAppSelector((state) => state.recos)
+    const [actualIndex, setActualIndex] = React.useState(0)
 
-    // actualIndex = 0
+    const renderRecoByIndex = (index: number) => {
+        if (hasIndex(userRecos, index)) {
+            return <UserReco reco={userRecos[index]} index={index} />
+        } else {
+            return <Text> TODO, contemplar casos para limites de la lista </Text>
+        }
+    }
+
+    const rightArrow = () => {
+        return (
+            <Pressable
+                style={{ position: "absolute", right: 0, width: 100, height: 450, zIndex: 2, alignItems: "flex-end", justifyContent: "center"}}
+                onPress={() => setActualIndex(actualIndex + 1)}
+            >
+                <Image
+                    source={LocalIcon.right}
+                    style={{ width: 30, height: 30 }}
+                />
+            </Pressable>
+        )
+    }
+    
+    const leftArrow = () => {
+        return (
+            <Pressable
+                style={{ position: "absolute", left: 0, width: 100, height: 450, zIndex: 2, alignItems: "flex-start", justifyContent: "center"}}
+                onPress={() => setActualIndex(actualIndex - 1)}
+            >
+                <Image
+                    source={LocalIcon.left}
+                    style={{ width: 30, height: 30 }}
+                />
+            </Pressable>
+        )
+    }
 
     return (
-        <ScrollView>
-            {userRecos.map((reco, index) => (
-                <UserReco reco={reco} index={index} key={index} />
-            ))}
+        <View style={{position: "relative"}}>
+            {renderRecoByIndex(actualIndex)}
+            {rightArrow()}
+            {leftArrow()}
             {/* <FlatList
                 ref={flatListRef}
                 horizontal
@@ -24,7 +68,7 @@ export const UserRecos = () => {
                 onScroll={handleScroll}
                 scrollEventThrottle={16}
             /> */}
-        </ScrollView>
+        </View>
     )
 }
 
@@ -34,7 +78,7 @@ const styles = StyleSheet.create({
     },
     poster: {
         width: "80%",
-        aspectRatio: 791/1186,
+        aspectRatio: 791 / 1186,
         borderRadius: 15,
         margin: 10,
     },

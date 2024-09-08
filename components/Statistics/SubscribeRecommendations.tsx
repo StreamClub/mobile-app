6d9ceb3d1f9db3b72668a9f6b5c9ba792665fa41
dbalestieri <dbalestieri@mulesoft.com>
2024-09-08 09:45:29 -0300

    add subscribe recommendations

--- components/Statistics/SubscribeRecommendations.tsx
+++ components/Statistics/SubscribeRecommendations.tsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { TitleText } from "../BasicComponents/TitleText";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { useGetSubscribeRecommendations } from "../../apiCalls/statistics";
+import { ProviderStatsType } from "./types/ProviderStatsType";
+import { TmdbImage, TmdbImageType } from "../BasicComponents/TmdbImage";
+import { BodyText } from "../BasicComponents/BodyText";
 
 export const SubscribeRecommendations = () => {
   const {getSubscribeRecommendations, loading} = useGetSubscribeRecommendations();
-  const [recommendations, setRecommendations] = useState([]);
+  const [recommendations, setRecommendations] = useState<Array<ProviderStatsType>>([]);
 
   const onSuccess = (response: any) => {
-    console.log(response.data)
+    setRecommendations(response.data.recommendations);
   }
 
   useEffect(() => {
@@ -19,6 +22,31 @@ export const SubscribeRecommendations = () => {
     <View>
       <TitleText
         body="Servicios Recomendados" />
+      {recommendations.map((item, index) => (
+        <View style={styles.platformRow} key={index}>
+          <TmdbImage
+            resource={item.logoPath}
+            type={TmdbImageType.Cover}
+            style={styles.logo} />
+          <BodyText 
+            body={item.providerName + ': a tus amigos les encanta este servicio'}
+            size='big'
+            style={{margin: 10, flex: 1}} />
+        </View>
+      ))}
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  logo: {
+    borderRadius: 20,
+    height: 80,
+    width: 80,
+    justifyContent: 'center'
+  },
+  platformRow: {
+    flexDirection: 'row',
+    margin: 10
+  }
+});
